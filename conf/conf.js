361e413da9f93ae8f7c3613d4ae740ba9922f333
Kateryna Kandziuba <katerynakandziuba@gmail.com>
2020-11-16 16:35:01 +0200

    Working tests on TS

--- conf/conf.js
+++ conf/conf.js
@@ -1,11 +1,11 @@
-import { Config } from "protractor";
+// import { Config } from "protractor";
+const tsNode = require('ts-node');
 
-export const config: Config =  {
+exports.config = {
     framework: 'jasmine',
     seleniumAddress: "http://localhost:4444/wd/hub",
-    // SELENIUM_PROMISE_MANAGER: false,
+    SELENIUM_PROMISE_MANAGER: false,
     directConnect: true,
-    noGlobals: false,
     capabilities: {
         browserName: "chrome",
         chromeOptions: {
@@ -21,5 +21,11 @@ export const config: Config =  {
 
     specs: [
         "C:/Users/1/Desktop/EPAM_JS/Kandziuba_EPAM_ProractorFramework/spec/contactFormTests.ts",
-    ]
+    ],
+
+    async onPrepare() {
+        tsNode.register({
+          project: './tsconfig.json',
+        });
+      },
 };
